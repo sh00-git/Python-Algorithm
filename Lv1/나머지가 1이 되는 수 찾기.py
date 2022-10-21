fc5a66f9b50f3592b040ea1ff8208160c3a6e957
Songhee Park <thdgml5539@naver.com>
2022-10-21 17:51:16 +0900

    221021 프로그래머스 Lv1. 2문제 링크 추가

--- Lv1/나머지가 1이 되는 수 찾기.py
+++ Lv1/나머지가 1이 되는 수 찾기.py
@@ -1,3 +1,4 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/87389
 def solution(n):
     min_num = 0
 
--- Lv1/음양 더하기.py
+++ Lv1/음양 더하기.py
@@ -1,3 +1,4 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/76501
 def solution(absolutes, signs):
     result = 0
     for i in range(len(absolutes)):
